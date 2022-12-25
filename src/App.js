5641cfc1f00219da52e38a9814a8094d7badb059
Manas <manastelavane999@gmail.com>
2022-12-25 22:59:56 +0530

    chat link changed

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import './App.css'
 
 import io from 'socket.io-client';
 
-const socket = io.connect('http://localhost:4000'); 
+const socket = io.connect('https://recipechatserver.onrender.com/'); 
 
 const App = () => {
   const user = JSON.parse(localStorage.getItem('profile'));
